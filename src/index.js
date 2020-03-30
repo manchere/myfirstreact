5d9e64ee0bafdff47cd4ba7c0d90b409fc1873b6
manchere <manucheremeh1995@gmail.com>
2020-03-30 19:29:19 +0000

    using react component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// var styler = {
+//   backgroundColor: 'orange',
+//   color: 'white',
+//   fontFamily: 'arial'
+// }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Message extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1 style ={{color: this.props.coloring}}>
+          { this.props.msg }
+        </h1>
+      </div>
+    )  
+  }
+}
+
+class SkiDayCounter extends React.Component{
+
+  getPercent = decimal =>{
+    return decimal * 100 + '%'
+  }
+
+  calcGoalProgress =(total, goal) =>{
+    return this.getPercent(total/goal)
+  }
+
+  render(){
+    const { total, powder, backcountry, goal } = this.props
+    return (
+      <section>
+        <div>
+          { backcountry }
+        </div>
+        <div>
+          { total }
+        </div>
+        <div>
+          { powder }
+        </div>
+      </section>
+    )
+  }
+}
+
+// const title = React.createElement( 
+//   'h1',t
+//   {id: 'title',className:'header', style: styler},
+//   'Hello World'
+// )
+
+let skiData = {
+  total: 50,
+  powder: 20,
+  backcountry: 10,
+  goal: 10
+}
+
+ReactDOM.render(<Message msg = "how are you" coloring = "blue" />,document.getElementById('root'))
+
+ReactDOM.render(<Message msg = "this is the second message" coloring= "red"/>,document.getElementById('stem'))
+
+ReactDOM.render( <SkiDayCounter total= { skiData.total } powder = { skiData.powder }
+backcountry = { skiData.backcountry } goal = { skiData.goal }/>,document.getElementById('root'))
+
