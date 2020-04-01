540d749d28a7b236cff1c57ad83f94709707deeb
manchere <manucheremeh1995@gmail.com>
2020-04-01 13:58:26 +0000

    modification made to index.js

--- src/index.js
+++ src/index.js
@@ -1,69 +1,118 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+import ReactDOM, { render } from 'react-dom'
 
-// var styler = {
-//   backgroundColor: 'orange',
-//   color: 'white',
-//   fontFamily: 'arial'
+// // var styler = {
+// //   backgroundColor: 'orange',
+// //   color: 'white',
+// //   fontFamily: 'arial'
+// // }
+
+// let skiData = {
+//   total: 50,
+//   powder: 20,
+//   backcountry: 10,
+//   goal: 10
 // }
 
-class Message extends React.Component{
-  render(){
+// const getPercent = decimal =>{
+//   return decimal * 100 + '%'
+// }
+
+// const calcGoalProgress =(total, goal) =>{
+//   return getPercent(total/goal)
+// }
+
+// class Message extends React.Component{
+//   render(){
+//     return (
+//       <div>
+//         <h1 style ={{color: this.props.coloring}}>
+//           { this.props.msg }
+//         </h1>
+//       </div>
+//     )  
+//   }
+// }
+
+// class SkiDayCounter extends React.Component{
+
+//   render(){
+//     const { total, powder, backcountry, goal } = this.props
+//     return (
+//       <section>
+//         <div>
+//           { backcountry }
+//         </div>
+//         <div>
+//           { total }
+//         </div>
+//         <div>
+//           { powder }
+//         </div>
+//       </section>
+//     )
+//   }
+// }
+
+// const Library  = () => {
+//   return (
+//     <div> 
+//       Welcome to the library
+//     </div>
+//   )
+  
+// }
+
+
+
+// render(<Message msg = "how are you" coloring = "blue" />,document.getElementById('root'))
+
+// render(<Message msg = "this is the second message" coloring= "red"/>,document.getElementById('stem'))
+
+// render( <SkiDayCounter total= { skiData.total } powder = { skiData.powder }
+
+// backcountry = { skiData.backcountry } goal = { skiData.goal }/>,document.getElementById('root'))
+
+// render( 
+// <div>
+//   <Library/>
+//   <Library/>
+// </div>
+// ,document.getElementById('stem'))
+
+const User = ({ name, lesson, id}) => {
+  return (
+  <section>
+    <p>{ name }</p>
+    <p>{ lesson }</p>
+    <p>{ id }</p>
+  </section>
+  )
+}
+
+let Userlist = [
+  {"name": "John Cena", "Lesson":"Ernest Senpai","id":260},
+  {"name": "Jerome Boabeng", "Lesson":"Eric Ries","id":293},
+  {"name": "George Getsrtrom", "Lesson":"John Stewart","id":260},
+]
+
+const Instructor = ({users}) => {
+  return (
+  <div>
+    {users.map((user, i) => <User key={i} name = {user.name} lesson = {user.Lesson} id = {user.id} /> ) } 
+  </div>
+  )
+}
+
+class Instructor extends React.Component {
+  render() {
+    const { books } = this.props.books
     return (
       <div>
-        <h1 style ={{color: this.props.coloring}}>
-          { this.props.msg }
-        </h1>
+        {users.map((user, i) => <User key={i} name = {user.name} lesson = {user.Lesson} id = {user.id} /> ) } 
       </div>
-    )  
+      )
   }
 }
 
-class SkiDayCounter extends React.Component{
-
-  getPercent = decimal =>{
-    return decimal * 100 + '%'
-  }
-
-  calcGoalProgress =(total, goal) =>{
-    return this.getPercent(total/goal)
-  }
-
-  render(){
-    const { total, powder, backcountry, goal } = this.props
-    return (
-      <section>
-        <div>
-          { backcountry }
-        </div>
-        <div>
-          { total }
-        </div>
-        <div>
-          { powder }
-        </div>
-      </section>
-    )
-  }
-}
-
-// const title = React.createElement( 
-//   'h1',t
-//   {id: 'title',className:'header', style: styler},
-//   'Hello World'
-// )
-
-let skiData = {
-  total: 50,
-  powder: 20,
-  backcountry: 10,
-  goal: 10
-}
-
-ReactDOM.render(<Message msg = "how are you" coloring = "blue" />,document.getElementById('root'))
-
-ReactDOM.render(<Message msg = "this is the second message" coloring= "red"/>,document.getElementById('stem'))
-
-ReactDOM.render( <SkiDayCounter total= { skiData.total } powder = { skiData.powder }
-backcountry = { skiData.backcountry } goal = { skiData.goal }/>,document.getElementById('root'))
-
+render(<Instructor users = { Userlist }/>,document.getElementById( 'root' ))
